9a1146fbba447919c12d8877c56024d0f2c3c788
M Ogrodniczak <mjogrodniczak@gmail.com>
2016-03-20 13:33:17 +0000

    ui.bootstrap

--- app/app.js
+++ app/app.js
@@ -2,30 +2,30 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ui.router',
-  'mo.resizer'
-]).
-config(['$stateProvider', function($stateProvider) {
-  $stateProvider
-      .state('index', {
-        url: "",
-        views: {
-          "viewA": { templateUrl: "view1/index.viewA.html" },
-          "viewB": { templateUrl: "view1/index.viewB.html" }
-        }
-      })
-      .state('route1', {
-        url: "/route1",
-        views: {
-          "viewA": { template: "route1.viewA" },
-          "viewB": { template: "route1.viewB" }
-        }
-      })
-      .state('route2', {
-        url: "/route2",
-        views: {
-          "viewA": { template: "route2.viewA" },
-          "viewB": { template: "route2.viewB" }
-        }
-      })
+    'ui.router',
+    'mo.resizer',
+    'ui.bootstrap'
+]).config(['$stateProvider', function ($stateProvider) {
+    $stateProvider
+        .state('index', {
+            url: "",
+            views: {
+                "viewA": {templateUrl: "view1/index.viewA.html"},
+                "viewB": {templateUrl: "view1/index.viewB.html"}
+            }
+        })
+        .state('route1', {
+            url: "/route1",
+            views: {
+                "viewA": {template: "route1.viewA"},
+                "viewB": {template: "route1.viewB"}
+            }
+        })
+        .state('route2', {
+            url: "/route2",
+            views: {
+                "viewA": {template: "route2.viewA"},
+                "viewB": {template: "route2.viewB"}
+            }
+        })
 }]);
