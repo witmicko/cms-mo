1a9e685818c88b5b7a0dabe375cf48f57bae6c87
M Ogrodniczak <mjogrodniczak@gmail.com>
2016-03-20 13:17:51 +0000

    splitter works

--- app/app.js
+++ app/app.js
@@ -2,11 +2,30 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'ui.router',
+  'mo.resizer'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+config(['$stateProvider', function($stateProvider) {
+  $stateProvider
+      .state('index', {
+        url: "",
+        views: {
+          "viewA": { templateUrl: "view1/index.viewA.html" },
+          "viewB": { templateUrl: "view1/index.viewB.html" }
+        }
+      })
+      .state('route1', {
+        url: "/route1",
+        views: {
+          "viewA": { template: "route1.viewA" },
+          "viewB": { template: "route1.viewB" }
+        }
+      })
+      .state('route2', {
+        url: "/route2",
+        views: {
+          "viewA": { template: "route2.viewA" },
+          "viewB": { template: "route2.viewB" }
+        }
+      })
 }]);
