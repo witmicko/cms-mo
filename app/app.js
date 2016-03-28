86d5cf9bfa7e2881a61af8ad2843a218cca5c258
M Ogrodniczak <mjogrodniczak@gmail.com>
2016-03-28 23:21:37 +0100

    meta styling

--- app/app.js
+++ app/app.js
@@ -36,37 +36,29 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
             url: '/meta',
             templateUrl: 'view1/builder/form-01-meta.html'
         })
-        .state('form.availability', {
-            url: '/availability',
-            templateUrl: 'view1/builder/form-02-availability.html'
-        })
-        .state('form.notes', {
-            url: '/notes',
-            templateUrl: 'view1/builder/form-03-notes.html'
-        })
         .state('form.header', {
             url: '/header',
-            templateUrl: 'view1/builder/form-04-header.html'
+            templateUrl: 'view1/builder/form-02-header.html'
         })
         .state('form.offerings', {
             url: '/offerings',
-            templateUrl: 'view1/builder/form-05-offerings.html'
+            templateUrl: 'view1/builder/form-03-offerings.html'
         })
         .state('form.organisation', {
             url: '/organisation',
-            templateUrl: 'view1/builder/form-06-organisation.html'
+            templateUrl: 'view1/builder/form-04-organisation.html'
         })
         .state('form.contact', {
             url: '/contact',
-            templateUrl: 'view1/builder/form-07-contact.html'
+            templateUrl: 'view1/builder/form-05-contact.html'
         })
         .state('form.attendees', {
             url: '/attendees',
-            templateUrl: 'view1/builder/form-08-attendees.html'
+            templateUrl: 'view1/builder/form-06-attendees.html'
         })
         .state('form.submission', {
             url: '/submission',
-            templateUrl: 'view1/builder/form-09-submission.html'
+            templateUrl: 'view1/builder/form-07-submission.html'
         })
        
 }]);
